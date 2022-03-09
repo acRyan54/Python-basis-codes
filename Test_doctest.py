f22c71a11b52f79e9cb5e8e66dc284dd8c8960df
Tsu <tsuthansing@qq.com>
2022-03-09 11:20:50 +0800

    3-9 11：20

--- Test_doctest.py
+++ Test_doctest.py
@@ -5,7 +5,7 @@ def fact(n):
     >>> fact(1)
     1
     >>> fact(10)
-    3628800
+    362880 
     >>> fact(-1)
     Traceback (most recent call last):
     ...
@@ -15,7 +15,7 @@ def fact(n):
         raise ValueError()
     if n == 1:
         return 1
-    assert n > 1, 'n is not greater than 1'
+    assert n > 1, 'n is not greater than 1'  # 断言
     return n * fact(n - 1)
 if __name__ == '__main__':
     import doctest
